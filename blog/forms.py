a3232d111f6b361361577e158f23f280175e49a9
kizil tims <sovietnord@gmail.com>
2018-01-24 17:07:23 +0300

    version 0.9

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Post
+from django.forms import ModelForm, Textarea
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('author', 'title', 'text',)
+        widgets = {
+            'text': Textarea(attrs={'cols': 18, 'rows': 2}),
+        }
+
+
+
+

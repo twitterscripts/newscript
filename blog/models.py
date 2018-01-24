7ab49d82609d47cf9083427bd6f1ef09eb1fddb0
kizil tims <sovietnord@gmail.com>
2018-01-24 11:52:33 +0300

    authors of the posts

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(

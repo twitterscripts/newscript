bcd36e82328f73599007df04029d5bed7fac9f20
twitterscripts <timourabel@gmail.com>
2018-02-01 01:07:40 +0300

    Nouveau Pack v0.1

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^feed/$', views.post_list, name='post_list'),
     url(r'^reports/$', views.report_list, name='report_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^report/new/$', views.report_new, name='report_new'),
-    url(r'^script/new/$', views.the_script, name='the_script'),
+    url(r'^$', views.the_script, name='the_script'),
     ]
